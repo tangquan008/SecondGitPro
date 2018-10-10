e7915b98e9002db4374a01701027e2e7a8ff5b07
wushichang <42518274+wushichang@users.noreply.github.com>
2018-10-10 15:44:55 +0800

    Update FirstClass.java
    try syncho

--- src/FirstClass.java
+++ src/FirstClass.java
@@ -3,5 +3,6 @@ public class FirstClass {
 	private String name1="tangquan";
 	private String name2="mapiquan";
 	private String name3="goushiquan";
-	private String name4="woertangquan��ʺȨ";
+	private String name4="woertangquan¹·ÊºÈ¨";
+	private String name5="i am a single lady";
 }
