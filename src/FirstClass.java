302700219890dea57b1a31cac6fd20b48a22eefb
wushichang <2937198847@qq.com>
2018-10-10 16:16:18 +0800

    本地修改了

--- src/FirstClass.java
+++ src/FirstClass.java
@@ -3,6 +3,5 @@ public class FirstClass {
 	private String name1="tangquan";
 	private String name2="mapiquan";
 	private String name3="goushiquan";
-	private String name4="woertangquan¹·ÊºÈ¨";
-	private String name5="i am a single lady";
+	private String name5="marry";
 }
